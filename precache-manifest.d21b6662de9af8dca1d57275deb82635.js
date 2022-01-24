048a622bf4bef661f7e115b79100619dbec50c80
Yuriy Gerasimenko <Yuriy.Gerasimenko@tatar.ru>
2022-01-24 23:36:56 +0300

    deploy

---
+++ precache-manifest.d21b6662de9af8dca1d57275deb82635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481f41498c25d3998a29",
+    "url": "css/app.4fcf963a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2516560c90dc8048272527aa627ae993",
+    "url": "index.html"
+  },
+  {
+    "revision": "481f41498c25d3998a29",
+    "url": "js/app.3a0fd03f.js"
+  },
+  {
+    "revision": "336fa74cf17ee5bc80d0",
+    "url": "js/chunk-vendors.1bfa2646.js"
+  },
+  {
+    "revision": "3868a87186592ee3caa21fc69400dc7f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
